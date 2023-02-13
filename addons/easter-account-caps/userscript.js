f4b90003b9a35857cc057f32b2a84ff87d916ac4
LercDsgn <75680333+LercDsgn@users.noreply.github.com>
2023-02-13 20:01:29 +0300

    Fix parantheses

--- addons/easter-account-caps/userscript.js
+++ addons/easter-account-caps/userscript.js
@@ -1,7 +1,7 @@
 export default async function ({ addon, global, console }) {
 
-  const accountSettings = addon.tab.waitForElement((".account-nav .dropdown > li:nth-child(3), ul.user-nav > li:nth-child(3), \
-  [class*='menu-bar_account-info-group_'] [class*='menu_menu_'] > [class*='menu_menu-item_']:nth-child(3), {
+  const accountSettings = addon.tab.waitForElement(".account-nav .dropdown > li:nth-child(3), ul.user-nav > li:nth-child(3), \
+  [class*='menu-bar_account-info-group_'] [class*='menu_menu_'] > [class*='menu_menu-item_']:nth-child(3)", {
 	  markAsSeen: false,
   }); 
   accountSettings.textContent = addon.settings.get("titleCase") === true ? "Account SettingS" : "Account settingS" 
